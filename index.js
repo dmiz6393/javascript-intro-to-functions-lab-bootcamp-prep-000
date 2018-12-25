f8eefdb6dba3dcf6b2b23bfdc25817af19ac1fe9
Danielle Mizrachi <dmiz@brandeis.edu>
2018-12-25 16:03:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  returns 
+  return "I can't hear you";
+}
+  if (string===string.toUpperCase);
 }
