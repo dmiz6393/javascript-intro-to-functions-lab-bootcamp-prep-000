0e8a7d14c29977ec07c8ebfeab874088a096d7a5
Danielle Mizrachi <dmiz@brandeis.edu>
2018-12-25 15:44:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ function whisper(string) {
   return string.toLowerCase();
 }
 function logShout(string) {
-  console.log(string.toUpperCase());
+  console.log(shout(string));
 } 
+function logWhisper(string) {
+  console.log(whisper(string));
+}
