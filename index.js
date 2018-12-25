408afdecfb1e572eff19a0b77f526820e9f65fd2
Danielle Mizrachi <dmiz@brandeis.edu>
2018-12-25 16:39:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function sayHiToGrandma(string) {
 else if (string===string.toUpperCase());
   return "YES INDEED!"; 
 }
-else (string=="I love you,Grandma.");
+else (string=="I love you,Grandma.");{
   return "I love you,too."
 }
